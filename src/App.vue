400b47fe02bb7a55a82e106e65bf407610a84d86
GioMench <giorgiaglm@gmail.com>
2024-03-20 15:46:03 +0100

    add message

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script>
-
-
+export default {
+    data(){
+        return{
+            text : 'this is Vite Vue'
+        }
+    }
+}
 </script>
 
 <template>
-
+ <div>
+    <h1>Hello World!</h1>
+    <p>{{ text }}</p>
+ </div>
 </template>
 
 <style>
